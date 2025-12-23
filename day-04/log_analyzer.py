fbd0f689cd9aab1fcff89e1632ebc2e03efce389
Afroz Shaikh <afsk1997@gmail.com>
2025-12-23 15:05:03 +0530

    Add log analyzer to count log messages
    This script reads a log file, counts INFO, WARNING, and ERROR messages, and writes the summary to a JSON file.

---
+++ day-04/log_analyzer.py
@@ -0,0 +1,41 @@
+#fun open file and read from file
+#identify info,warning,error messages and count them
+#print the summary and write it to a file
+import json
+count = {"INFO":0, "WARNING":0, "ERROR":0}
+
+def open_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            if file.readlines() == []:
+                print("The log file is empty.")
+                exit()
+            return file.readlines()
+    except Exception as e:
+        print(f"Error opening file: {e}")
+        exit()
+        return []
+
+def analyze_logs(file_data):
+    global count
+    for line in file_data:
+        if "INFO" in line:
+            count["INFO"] += 1
+        elif "WARNING" in line:
+            count["WARNING"] += 1
+        elif "ERROR" in line:
+            count["ERROR"] += 1
+            
+def wrtie_output():
+    try:
+        with open("log_summary.json","w") as f:
+            json.dump(count,f,indent=2)
+    except Exception as e:
+        print(f"Error writing to file: {e}")
+
+f_name=input("Enter log file name: ")
+file_data=open_file(f_name)
+analyze_logs(file_data)
+print(f"Total INFO : {count['INFO']}\nTotal WARNINGS : {count['WARNING']}\nTotal ERRORS : {count['ERROR']}")
+wrtie_output()
+
